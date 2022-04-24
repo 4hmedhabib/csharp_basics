f2007c08c682d7152a41e9d621e181b599e5438f
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-04-24 07:51:30 +0300

    added: if statement

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            int c = 2;
+
+            // if (a + b > 10)
+            // {
+            //     Console.WriteLine("The answer is greater than 10.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("The answer is less than 10.");
+            // }
+
+            // if ((a + b + c > 10) && (a == b))
+            // {
+            //     Console.WriteLine("The answer is greater than 10");
+            //     Console.WriteLine("And the first number is equal to the second.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("The answer is not greater than 10");
+            //     Console.WriteLine("Or the first number is not equal to the second");
+            // }
+
+            // if ((a + b + c > 10) || (a == b))
+            // {
+            //     Console.WriteLine("The answer is greater than 10");
+            //     Console.WriteLine("Or the first number is equal to the second");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("The answer is not greater than 10");
+            //     Console.WriteLine("And the first number is not equal to the second");
+            // }
+        }
+    }
+}
