aaea07a747af2ce8678219ebf0b3bf9d586514eb
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-04-24 09:50:04 +0300

    added: loops

--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            int a = 5;
-            int b = 3;
-            int c = 2;
+            // int a = 5;
+            // int b = 3;
+            // int c = 2;
+
+            // * IF STATEMENT
+            //   ============
 
             // if (a + b > 10)
             // {
@@ -42,6 +45,85 @@ namespace ConsoleApp6
             //     Console.WriteLine("The answer is not greater than 10");
             //     Console.WriteLine("And the first number is not equal to the second");
             // }
+
+            // * LOOPS
+            // =======
+
+            // ? WHILE LOOP
+            // ============
+
+            // int counter = 0;
+            // while (counter < 10)
+            // {
+            //     Console.WriteLine(counter);
+            //     counter++;
+            // }
+
+            // ? DO WHILE LOOP
+            // ===============
+            // do
+            // {
+            //     Console.WriteLine(counter);
+            //     counter++;
+            // } while (counter < 10);
+
+
+            // ? FOR LOOP
+            // ==========
+            // for (int index = 1; index <= 10; index++)
+            // {
+            //     Console.WriteLine($"Hello World, {index}");
+            // }
+
+            // Nested If Statement and Loops
+            // =============================
+            // int sum = 0;
+            // for (int i = 1; i <= 10; i++)
+            // {
+            //     if (i % 3 == 0)
+            //     {
+            //         sum = sum + i;
+            //     }
+            // }
+            // Console.WriteLine($"Total Sum is : {sum}");
+
+            // Nested Loops
+            // ============
+            // for (int row = 1; row < 11; row++)
+            // {
+            //     for (char column = 'a'; column < 'k'; column++)
+            //     {
+            //         Console.WriteLine($"The cell is ({row}, {column})");
+            //     }
+            // }
+
+            // ? FOREACH LOOP
+            // ==============
+            var names = new List<string> {
+                "name",
+                "Ahmed"
+            };
+            // foreach (string name in names)
+            // {
+            //     Console.WriteLine(name.ToUpper());
+            // }
+
+            // for (int i = 0; i < names.Count; i++)
+            // {
+            //     Console.WriteLine($"{names[i].ToLower()} ======");
+            // }
+
+            // ARRAY METHODS
+            // =============
+            // names.Add("Maria");
+            // names.Add("Bill");
+            // names.Remove("Ahmed");
+
+            // foreach (string name in names)
+            // {
+            //     Console.WriteLine(name.ToUpper());
+            // }
+
         }
     }
 }
