2c591b0bbb61f4d210f1fd67b054b1d71f47712e
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-05-22 18:05:13 +0300

    updated: BankApp

--- bankApp/Program.cs
+++ bankApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Ahmed Habib", 10000);
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+
+    }
+}
