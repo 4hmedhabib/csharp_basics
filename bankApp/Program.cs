e1fabc55aefe6a34ec9c55bd065fac022ff2f953
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-05-22 21:47:25 +0300

    updated: bankApp

--- bankApp/Program.cs
+++ bankApp/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
 
-namespace BankApp
+namespace MySuperBank
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             var account = new BankAccount("Ahmed Habib", 10000);
             account.MakeWithdrawal(120, DateTime.Now, "Hammock");
             account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
--- bankApp/Transaction.cs
+++ bankApp/Transaction.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace BankApp
+namespace MySuperBank
 {
     public class Transaction
     {
