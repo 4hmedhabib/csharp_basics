dfc0082de2086ce5442220673c17cbafb60f1adf
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-05-22 21:47:39 +0300

    added: Asp.net

---
+++ asp.net/Models/Product.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ContosoCrafts.Website.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+
+    }
+}
